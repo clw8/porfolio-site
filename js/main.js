f2c7533ab7369defb84780aab70c3ee18fdec959
Chris Walsh <chris.w894j@gmail.com>
2018-11-11 01:23:42 +0100

    responsive design css changes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import interfaceOperations from './interface.js';
 //import Typed from '../node_modules/typed.js/src/typed.js';
 
 //uncomment below for webpack
-  import '../css/styles.css';
+  // import '../css/styles.css';
 
 nodesWrapper();
 modalsWrapper();
@@ -31,17 +31,26 @@ setTimeout(()=>{
 //contact form
 const contactSubmit = document.querySelector('#contact-submit');
 const formInputs = document.querySelectorAll('.form-input');
+
 contactSubmit.addEventListener('click', (e)=> {
+  let filled = true;
+  for (let i = 0; i < formInputs.length; ++i) {
+    if ( !!formInputs[i].value == false ) {
+      filled = false;
+    }
+  }
+  if( filled === true ) { 
     e.preventDefault();
+
     //json triggers preflight, so we are manually serializing the input data rather than sending as a json object
-   let data = "";
+    let data = "";
     for (let i = 0; i < formInputs.length; ++i) {
       let input = formInputs[i];
       if (input.name) {
-      data += `${input.value}%$`;
+        data += `${input.value}%$`;
       }
     }
-
+    
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -53,13 +62,18 @@ contactSubmit.addEventListener('click', (e)=> {
         document.getElementById("submit-res").classList.add('error');
       }
     };
-
-   const url = "https://script.google.com/macros/s/AKfycbyuFIEsLSei7wgbmO4aUuurv_5xAypuyqweNQYrdZNY01pQ7vg/exec";
-   xhttp.open("POST", url, true);
+    
+    const url = "https://script.google.com/macros/s/AKfycbyuFIEsLSei7wgbmO4aUuurv_5xAypuyqweNQYrdZNY01pQ7vg/exec";
+    xhttp.open("POST", url, true);
     xhttp.setRequestHeader('Content-Type', 'text/plain');
-   xhttp.setRequestHeader('Accept', 'text/plain');
+    xhttp.setRequestHeader('Accept', 'text/plain');
     xhttp.send(data);
+  }
 })
+
+
+
+
 
 
 
